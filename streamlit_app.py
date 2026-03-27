e3593e2124aee1d47d642786f6f0c9b0922402e4
ktoshniwal4 <ktoshniwal4@gmail.com>
2026-03-27 12:17:53 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from matplotlib.ticker import PercentFormatter
 # PAGE CONFIG
 # ──────────────────────────────────────────────────────────
 st.set_page_config(
-    page_title="EcoVest · Sustainable Portfolio",
+    page_title="Ethos-Invest · Sustainable Portfolio",
     page_icon="🌿",
     layout="wide",
     initial_sidebar_state="expanded",
